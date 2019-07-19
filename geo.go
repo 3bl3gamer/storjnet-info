87fa91481dfde2a27d5db5f12e3ef7c11ea26948
3bl3gamer <3bl3gamer@gmail.com>
2019-07-20 00:55:41 +0300

    Resolving nodes addresses (for geo location)

--- geo.go
+++ geo.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"net"
+	"strings"
+
 	"github.com/abh/geoip"
 	"storj.io/storj/pkg/pb"
 )
+
+func splitToHostAndPort(addr string) (string, string) {
+	index := strings.LastIndexByte(addr, ':')
+	if index == -1 {
+		return "", ""
+	}
+	return addr[:index], addr[index+1:]
+}
 
 func StartLocationSearcher(gdb *geoip.GeoIP, kadDataRawChan chan *pb.Node, kadDataForSaveChan chan *KadDataExt) Worker {
 	worker := NewSimpleWorker(1)
@@ -12,8 +23,18 @@ func StartLocationSearcher(gdb *geoip.GeoIP, kadDataRawChan chan *pb.Node, kadDa
 		defer worker.Done()
 		for nodeRaw := range kadDataRawChan {
 			node := &KadDataExt{Node: nodeRaw, Location: nil}
-			if nodeRaw.LastIp != "" {
-				if rec := gdb.GetRecord(nodeRaw.LastIp); rec != nil {
+			ip := nodeRaw.LastIp
+			if ip == "" {
+				host, _ := splitToHostAndPort(nodeRaw.Address.Address)
+				ips, err := net.LookupHost(host)
+				if err == nil {
+					ip = ips[0]
+				} else {
+					logWarn("GEO-LOOKUP", "addr '%s' lookup error: %s", nodeRaw.Address.Address, err)
+				}
+			}
+			if ip != "" {
+				if rec := gdb.GetRecord(ip); rec != nil {
 					// fmt.Printf("%#v\n", rec)
 					node.Location = &NodeLocation{
 						Country:   rec.CountryName,
