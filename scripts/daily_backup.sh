76483245427c8a9f325c02dd2839d416d9f409db
3bl3gamer <3bl3gamer@gmail.com>
2020-06-25 17:38:46 +0300

    Daily backup with cleanup

--- scripts/daily_backup.sh
+++ scripts/daily_backup.sh
@@ -2,4 +2,6 @@
 scripts_dirname=`dirname "$0"`
 dumps_dirname=${1:-$scripts_dirname/../dumps}
 mkdir -p "$dumps_dirname"
-"$scripts_dirname/db_dump.sh" "$dumps_dirname/dump_`date -Idate`.sql.gz"
+stamp=`date -Idate`
+"$scripts_dirname/db_dump.sh" "$dumps_dirname/dump_$stamp.sql.gz"
+find "$dumps_dirname" -type f ! -regex '.*/dump.*-\(01\|15\|28\).sql.gz' ! -name "dump_$stamp.sql.gz" -exec rm {} \;
