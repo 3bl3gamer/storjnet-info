9ec777fbb79c43ed1ac569dab030448f841013a5
3bl3gamer <3bl3gamer@gmail.com>
2022-04-04 00:07:18 +0300

    Timeout fix

--- updater/updater.go
+++ updater/updater.go
@@ -23,11 +23,11 @@ type UserNodeWithErr struct {
 }
 
 func doPing(sats utils.Satellites, node *core.Node) (time.Duration, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
 	var lastErr error
 	for _, sat := range sats {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
 		stt := time.Now()
 		conn, err := sat.Dial(ctx, node.Address, node.ID, utils.SatModeTCP)
 		if err != nil {
